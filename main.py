5931c3ba392402c9030ab72f99e1984947433cef
denefee <dsher2004@gmail.com>
2023-02-27 01:18:57 +0300

    some physics realised

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ import os
 N = int(1000) # количество частиц
 Mass = int(1) # масса материи
 # m = float(Mass/N) # масса одной частицы
+# m = float(Mass/N) # масса одной частицы
 Vmax = int(1)  # максимальная скорость частицы
 # d = float(0.01) # delta-окрестность
+# d = float(0.01) # delta-окрестность
 dt = float(0.001) # тик
+Leng = int(10) # длина коробки
 Leng = int(10) # длина коробки
 
 
@@ -21,6 +24,8 @@ class Particle:
     self.c = c
     self.v = v
     self.a = a
+    self.lc = c
+
     self.lc = c
 
   def display(self):
@@ -43,6 +48,7 @@ def rand_gen_char(pars):
   n += 1
   print (n)
   return Particle(c, v)
+  
   
   
 def rand_gen(pars):
@@ -70,6 +76,7 @@ def cell_gen(pars):
         z = k*dl
         n += 1
         c = np.array([x, y, z])
+        v = np.random.uniform(-Vmax, Vmax, (3))
         v = np.random.uniform(-Vmax, Vmax, (3))
         print (n)
         pars.append(Particle(c, v))
@@ -126,15 +133,69 @@ def timego(pars, tick):
     calc_ax(pars)
     move(pars)
     Particle.display(pars[N//2])
+def axel(part, part1):
+  # считает силы взаимодействия между данными частицами и меняет их ускорения
+  vecr = part1.c - part.c
+  modr = np.linalg.norm(vecr)
+  ac = (24/(modr**8) - 48/(modr**14))*vecr
+  part.a = part.a + ac
+  part1.a = part1.a - ac
+  
+  
+def calc_ax(pars):
+  # вычисляет ускорения всех частиц
+  for i in np.arange(N-1):
+    for j in np.arange(i+1, N):
+      axel(pars[i], pars[j])
+      
+
+def first_move_one(part):
+  # двигает частицу в первый раз
+  part.c = part.c + part.v*dt + 0.5*(dt**2)*part.a
 
 
+def first_move(pars):
+  # двигает все частицы в первый раз
+  for i in np.arange(N):
+    first_move_one(pars[i])
+
+
+def move_one(part):
+  #двигает частицу используя схему Верле
+  mem = part.c
+  part.c = 2*part.c - part.lc + (dt**2)*part.a
+  part.lc = mem
+  part.v += part.a*dt
+    
+    
+def move(pars):
+  #двигает все частицы
+  for i in np.arange(N):
+    move_one(pars[i])
+   
+    
+def timego(pars, tick):
+  first_move(pars)
+  for i in np.arange(tick - 1):
+    print(i)
+    calc_ax(pars)
+    move(pars)
+    Particle.display(pars[N//2])
+
+
+def main():  
+  zeit = int(100)
 def main():  
   zeit = int(100)
   start = time.time() # точка отсчета времени
   pars = []
   cell_gen(pars) # генерация сеткой
   Particle.display(pars[N//2])
+  Particle.display(pars[N//2])
   # rand_gen(pars) # случайная генерация, пока не работает
+  #for i in np.arange(N): # выводит характеристики всех частиц
+  #  Particle.display(pars[i])
+  timego(pars, zeit)
   #for i in np.arange(N): # выводит характеристики всех частиц
   #  Particle.display(pars[i])
   timego(pars, zeit)
