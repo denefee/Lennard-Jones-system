e2adae6ed5909a26ffb5d01d89537d3e5c6f97e4
Denis <116732042+denefee@users.noreply.github.com>
2023-02-27 01:23:48 +0300

    physics realised

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ import os
 # глобальные переменные
 N = int(1000) # количество частиц
 Mass = int(1) # масса материи
-m = float(Mass/N) # масса одной частицы
+# m = float(Mass/N) # масса одной частицы
 Vmax = int(1)  # максимальная скорость частицы
-d = float(0.01) # delta-окрестность
+# d = float(0.01) # delta-окрестность
 dt = float(0.001) # тик
-eps = int(1) # потенциальная яма
-sig = int(1) # расстояние, на котором энергия взаимодействия обнуляется
-Leng = 100*sig # длина коробки
+Leng = int(10) # длина коробки
 
 
 class Particle:
@@ -23,19 +21,7 @@ class Particle:
     self.c = c
     self.v = v
     self.a = a
- 
-  def pot_calc(self):
-    U = 4*eps()
-    return 0
-
-  def force(self):
-    return 0
-
-  def movement(self):
-    return 0
-
-  def potential(self):
-    return 0
+    self.lc = c
 
   def display(self):
     return print('Координаты: ' + np.array2string(self.c) + 
@@ -57,6 +43,7 @@ def rand_gen_char(pars):
   n += 1
   print (n)
   return Particle(c, v)
+  
   
 def rand_gen(pars):
   # генерирует N частиц с уникальной рандомной координатой
@@ -83,7 +70,7 @@ def cell_gen(pars):
         z = k*dl
         n += 1
         c = np.array([x, y, z])
-        v = np.random.uniform(-Vmax, Vmax, (1, 3))
+        v = np.random.uniform(-Vmax, Vmax, (3))
         print (n)
         pars.append(Particle(c, v))
         if (n == N):
@@ -91,28 +78,69 @@ def cell_gen(pars):
           break
 
 
+def axel(part, part1):
+  # считает силы взаимодействия между данными частицами и меняет их ускорения
+  vecr = part1.c - part.c
+  modr = np.linalg.norm(vecr)
+  ac = (24/(modr**8) - 48/(modr**14))*vecr
+  part.a = part.a + ac
+  part1.a = part1.a - ac
+  
+  
+def calc_ax(pars):
+  # вычисляет ускорения всех частиц
+  for i in np.arange(N-1):
+    for j in np.arange(i+1, N):
+      axel(pars[i], pars[j])
+      
 
-def pot():
-  Ux = 0
-  Uy = 0
-  Uz = 0
-
-  U = np.array(Ux, Uy, Uz)
-  return U
+def first_move_one(part):
+  # двигает частицу в первый раз
+  part.c = part.c + part.v*dt + 0.5*(dt**2)*part.a
 
 
-def main():
+def first_move(pars):
+  # двигает все частицы в первый раз
+  for i in np.arange(N):
+    first_move_one(pars[i])
+
+
+def move_one(part):
+  #двигает частицу используя схему Верле
+  mem = part.c
+  part.c = 2*part.c - part.lc + (dt**2)*part.a
+  part.lc = mem
+  part.v += part.a*dt
+    
+    
+def move(pars):
+  #двигает все частицы
+  for i in np.arange(N):
+    move_one(pars[i])
+   
+    
+def timego(pars, tick):
+  first_move(pars)
+  for i in np.arange(tick - 1):
+    print(i)
+    calc_ax(pars)
+    move(pars)
+    Particle.display(pars[N//2])
+
+
+def main():  
+  zeit = int(100)
   start = time.time() # точка отсчета времени
   pars = []
   cell_gen(pars) # генерация сеткой
+  Particle.display(pars[N//2])
   # rand_gen(pars) # случайная генерация, пока не работает
-  for i in np.arange(N): # выводит характеристики всех частиц
-    Particle.display(pars[i])
+  #for i in np.arange(N): # выводит характеристики всех частиц
+  #  Particle.display(pars[i])
+  timego(pars, zeit)
   end = time.time() - start # собственно время работы программы
   print(end) # вывод времени
 
 
 if __name__ == "__main__":
   main()
-  
-  # what
